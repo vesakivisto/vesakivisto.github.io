2e5d318a2eb4b9ff880cba8dad5237852084fef6
vesakivisto <kivisto.vesku@gmail.com>
2017-01-25 16:15:51 +0200

    Change portfolio title

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -670,7 +670,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("portfolio/app")["default"].create({"name":"portfolio","version":"0.0.0+e0904708"});
+  require("portfolio/app")["default"].create({"name":"portfolio","version":"0.0.0+8d1679d7"});
 }
 
 /* jshint ignore:end */
