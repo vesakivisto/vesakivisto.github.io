0a3fdd0f3cc7eaf4df55bffac80b31596e723a8a
kivive <vesa.kivisto@jamk.fi>
2019-07-26 11:14:56 +0300

    Quick fix.

--- src/components/About.js
+++ src/components/About.js
@@ -11,8 +11,8 @@ class About extends React.Component {
         <div className="twelve columns">
           <img className="profile-img" src="me.jpg" alt="project-img" />
           <h5 className="about">Hello!</h5>
-          <p>I'm Vesa Kivistö, an aspiring developer ready to conquer the world. I'm mostly interested in web development, though lately I have gained interest in mobile development
-            and artificial intelligence as well. I have experience in full-stack development and I'm always looking forward to learning something new.</p>
+          <p>I'm Vesa Kivistö, a web developer ready to conquer the world. I have experience in full-stack development, and lately I have gained interest in mobile development
+            and artificial intelligence as well. I'm always looking forward to learning something new.</p>
           <p>I've always been interested in information and communications technology as well as programming, which led to me starting to study software development at
             Saimaa Vocational College Sampo. After graduation from Sampo with a Vocational Qualification and a Matriculation Examination Certificate, I started my studies in
             software engineering at JAMK University of Applied Sciences, from which I graduated in December 2017 with a Bachelor's Degree. I plan on studying data analytics and machine
